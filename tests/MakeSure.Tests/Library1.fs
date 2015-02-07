a45ec23e6687a6cdc326fbbe06b3e49a85515d89
Gagege <g@gageh.us>
2015-02-07 13:15:27 -0800

    Switched test framework to xUnit

--- tests/MakeSure.Tests/Library1.fs
+++
@@ -1,4 +0,0 @@
-﻿namespace MakeSure.Tests
-
-type Class1() = 
-    member this.X = "F#"
---
+++ tests/MakeSure.Tests/Validation.fs
@@ -0,0 +1,10 @@
+﻿namespace MakeSure.Tests
+
+open Xunit
+open Swensen.Unquote.Assertions
+
+module Validation =
+
+    [<Fact>]
+    let ``We are in the correct universe`` () =
+        test <@ 1 + 1 = 2 @>
