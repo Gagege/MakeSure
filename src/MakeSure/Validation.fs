8bfc3defb9605c31a5f77931bdc325d427af0ef7
Gagege <g@gageh.us>
2015-02-09 19:15:17 -0800

    Added makeSure function

--- src/MakeSure/Validation.fs
+++ src/MakeSure/Validation.fs
@@ -1,4 +1,12 @@
 ﻿namespace MakeSure
 
+open MakeSure.Utilities
+
 module Validation =
     
+    // run the validationFunction on the input
+    // if valid, return Success with validated input
+    // if not valid, return Failure with failureMessage
+    let makeSure input validationFunction failureMessage =
+        if validationFunction input then Success input
+        else Failure failureMessage
