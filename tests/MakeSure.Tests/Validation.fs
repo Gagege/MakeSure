585b19ae1086ee84e2c6b32ad4e39bb48262fb6f
Gage Herrmann <g@gageh.us>
2015-02-10 13:26:51 -0600

    Re-added tests for hasValue and makeSure functions

--- tests/MakeSure.Tests/Validation.fs
+++ tests/MakeSure.Tests/Validation.fs
@@ -9,3 +9,14 @@ module Validation =
     [<Fact>]
     let ``We are in the correct universe`` () =
         Assert.Equal(1 + 1, 2)
+
+    [<Fact>]
+    let ``If valid input is passed to "makeSure" it returns a Success result that matches input`` () =
+        let validInput = "test input"
+        Assert.Equal(makeSure validInput hasValue "Test failure message", Success validInput)
+
+    [<Fact>]
+    let ``If invalid input is passed to "makeSure" it returns a Failure result that matches failureMessage`` () =
+        let invalidInput = ""
+        let failureMessage = "Test failure message"
+        Assert.Equal(makeSure invalidInput hasValue failureMessage, Failure failureMessage)
