7f10c81040499b3339946dc750366f7fdfdb9f79
Gagege <g@gageh.us>
2015-02-09 21:32:05 -0800

    Added ifNotEmptyMakeSure function

--- src/MakeSure/Validation.fs
+++ src/MakeSure/Validation.fs
@@ -18,3 +18,10 @@ module Validation =
     let makeSure input validationFunction failureMessage =
         if validationFunction input then Success input
         else Failure failureMessage
+        
+    // if input is not empty (eg. "hasValue input = false"), run the validationFunction on the input
+    // if valid, return Success with validated input
+    // if not valid, return Failure with failureMessage
+    let ifNotEmptyMakeSure input validationFunction failureMessage =
+        if hasValue input = false || validationFunction input then Success input
+        else Failure failureMessage
