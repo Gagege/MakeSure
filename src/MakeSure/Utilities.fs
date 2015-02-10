1f264fab262aee26d032658cb5e8b5bfd76c77ef
Gagege <g@gageh.us>
2015-02-09 19:13:56 -0800

    Added Utilities.fs

---
+++ src/MakeSure/Utilities.fs
@@ -0,0 +1,76 @@
+﻿namespace MakeSure
+
+module Utilities =
+    
+     // the two-track type
+    type Result<'TSuccess,'TFailure> = 
+        | Success of 'TSuccess
+        | Failure of 'TFailure
+
+    // convert a single value into a two-track result
+    let succeed x = 
+        Success x
+
+    // convert a single value into a two-track result
+    let fail x = 
+        Failure x
+
+    // appy either a success function or failure function
+    let either successFunc failureFunc twoTrackInput =
+        match twoTrackInput with
+        | Success s -> successFunc s
+        | Failure f -> failureFunc f
+
+
+    // convert a switch function into a two-track function
+    let bind f = 
+        either f fail
+
+    // pipe a two-track value into a switch function 
+    let (>>=) x f = 
+        bind f x
+
+    // compose two switches into another switch
+    let (>=>) s1 s2 = 
+        s1 >> bind s2
+
+    // convert a one-track function into a switch
+    let switch f = 
+        f >> succeed
+
+    // convert a one-track function into a two-track function
+    let map f = 
+        either (f >> succeed) fail
+
+    // convert a dead-end function into a one-track function
+    let tee f x = 
+        f x; x 
+
+    // convert a one-track function into a switch with exception handling
+    let tryCatch f exnHandler x =
+        try
+            f x |> succeed
+        with
+        | ex -> exnHandler ex |> fail
+
+    // convert two one-track functions into a two-track function
+    let doubleMap successFunc failureFunc =
+        either (successFunc >> succeed) (failureFunc >> fail)
+
+    // add two switches in parallel
+    let plus addSuccess addFailure switch1 switch2 = 
+        match (switch1),(switch2) with
+        | Success s1,Success s2 -> Success (addSuccess s1 s2)
+        | Failure f1,Success _  -> Failure f1
+        | Success _ ,Failure f2 -> Failure f2
+        | Failure f1,Failure f2 -> Failure (addFailure f1 f2)
+
+    // Combine two validation functions
+    // uses "plus" function which is located in MakeSure.Utilities
+    // if both validation functions are of type Success, just return a Success containint first valid input
+    // if one or both validation functions is/are of type Failure, concatenate the failure message(s) 
+    //  and return a Failure containing the concatenated failure messages
+    let (&&&) v1 v2 = 
+        let addSuccess r1 r2 = r1 // return first
+        let addFailure s1 s2 = s1 + "; " + s2  // concat
+        plus addSuccess addFailure v1 v2
